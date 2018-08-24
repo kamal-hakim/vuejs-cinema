b98c3ee28dc23406ff53d6f31f8f931a9a171169
Kamal Hakim <kamalhakim.zainal@gmail.com>
2018-08-24 17:27:36 +0800

    add components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,41 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres';
 
 new Vue({
     el: "#app",
-    data: {
-        msg: 'hello world'
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
+                        <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                        </div>`,
+            data: function() {
+                return {
+                    movies: [
+                    { title: 'Pulp Fiction' },
+                    { title: 'Home Alone' },
+                    { title: 'Austin Powers' }
+                    ]
+                }
+                }
+        },
+        'movie-filter': {
+            data: function() {
+                return { 
+                    genres 
+                };
+            },
+            template: `<div id="movie-filter">
+                        <h2>Filter results</h2>
+                            <div class="filter-group">
+                                <check-filter v-for="genre in genres"></check-filter>
+                            </div>
+                        </div>`,
+            components: {
+                'check-filter': {
+                    template: '<div>Filter</div>'
+                }
+            }
+        }
     }
 });
